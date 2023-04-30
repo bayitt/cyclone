2289281914bad0e30fe2b9d9a3b1d278347b762a
Fambegbe Olamileke <olamileke.f@gmail.com>
2023-04-30 15:53:21 +0100

    feat: implemented authentication

--- cyclone/middleware/session.py
+++ cyclone/middleware/session.py
@@ -2,6 +2,5 @@ from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 from os import environ
 
-
 def add_session_middleware(app: FastAPI):
     app.add_middleware(SessionMiddleware, secret_key=environ.get("SECRET_KEY"))
