0c8c624f1be821f558928864b437f63323ff14d0
Fambegbe Olamileke <olamileke.f@gmail.com>
2023-04-30 16:29:28 +0100

    feat: working on application crud

--- cyclone/database/setup.py
+++ cyclone/database/setup.py
@@ -3,7 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from os import environ
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{environ.get("DATABASE_USER")}:{environ.get("DATABASE_PASSWORD")}@{environ.get("DATABASE_HOST")}:{environ.get("DATABASE_PORT")}/{environ.get("DATABASE_NAME")}'
+from ..utilities.env import load_env
+
+load_env()
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{environ.get("DATABASE_USER")}:{environ.get("DATABASE_PASSWORD")}@{environ.get("DATABASE_HOST")}:{int(environ.get("DATABASE_PORT"))}/{environ.get("DATABASE_NAME")}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- cyclone/middleware/session.py
+++ cyclone/middleware/session.py
@@ -2,5 +2,6 @@ from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 from os import environ
 
+
 def add_session_middleware(app: FastAPI):
     app.add_middleware(SessionMiddleware, secret_key=environ.get("SECRET_KEY"))
